2054599f53dea27f59966550f8f1561f49a63c13
hunderaweke <hunderaweke@gmail.com>
2023-10-15 23:13:09 -0900

    updated: button layout

---
+++ online-shopping/src/Buttons.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Buttons extends JButton {
+    public Buttons(String text) {
+        super(text);
+        this.setBackground(new Color(19, 126, 217));
+        // this.setFont(new Font("Poppins", Font.BOLD, 16));
+    }
+}
