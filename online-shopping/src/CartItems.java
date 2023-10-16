15e57ecb530b100562ed401d7eca159ce9a5e1b3
hunderaweke <hunderaweke@gmail.com>
2023-10-16 10:32:40 -0900

    updated: main ui

--- online-shopping/src/CartItems.java
+++ online-shopping/src/CartItems.java
@@ -5,7 +5,6 @@ import java.awt.*;
 import java.sql.*;
 
 public class CartItems extends JPanel {
-    private JButton addItemButton;
     private JButton removeItemButton;
     String connectionUrl = "jdbc:sqlserver://localhost:1433;Database=Online_shopping;user=hundera;password=55969362;encrypt=true;trustServerCertificate=true;";
 
@@ -16,7 +15,6 @@ public class CartItems extends JPanel {
             var cartTable = new JTable();
             var model = new DefaultTableModel();
             model.setColumnIdentifiers(new Object[] { "Item", "Price" });
-            addItemButton = new JButton("Add Item");
             removeItemButton = new JButton("Remove Item");
             var getCartItemsQuery = "SELECT cart_id, product_id FROM Cart_Item;";
             var getProductQuery = "SELECT product_name, price FROM Product WHERE product_id = ?;";
@@ -39,42 +37,18 @@ public class CartItems extends JPanel {
             }
             JPanel buttonPanel = new JPanel();
             buttonPanel.setLayout(new FlowLayout());
-            buttonPanel.add(addItemButton);
             buttonPanel.add(removeItemButton);
             cartTable.setModel(model);
             add(new JScrollPane(cartTable), BorderLayout.CENTER);
             add(buttonPanel, BorderLayout.SOUTH);
-
-            addItemButton.addActionListener(e -> {
-                String item = JOptionPane.showInputDialog("Enter item name:");
-                if (item != null && !item.isEmpty()) {
-                    // Add item to database
-                    try {
-                        PreparedStatement stmt = con
-                                .prepareStatement("INSERT INTO Cart_Item (cart_id, product_id) VALUES (?, ?)");
-                        stmt.setInt(1, 1);
-                        stmt.setInt(2, 1);
-                        stmt.executeUpdate();
-                    } catch (SQLException ex) {
-                        ex.printStackTrace();
-                    }
-                }
-            });
-
-            // Add action listener to removeItemButton
             removeItemButton.addActionListener(e -> {
-
-                // Get selected row
-                int row = cartTable.getSelectedRow();
-
+                int row = (int) cartTable.getSelectedRow();
                 if (row != -1) {
-
                     // Get product id from selected row
-                    int productId = (int) cartTable.getValueAt(row, 0);
-
-                    // Remove from database
+                    var productId = Integer.parseInt((String) cartTable.getValueAt(row, 0));
                     try {
-                        PreparedStatement stmt = con.prepareStatement("DELETE FROM Cart_Item WHERE product_id = ?");
+                        Connection conn = DriverManager.getConnection(this.connectionUrl);
+                        PreparedStatement stmt = conn.prepareStatement("DELETE FROM Cart_Item WHERE product_id = ?");
                         stmt.setInt(1, productId);
                         stmt.executeUpdate();
                         model.removeRow(row);
@@ -84,10 +58,8 @@ public class CartItems extends JPanel {
                 }
             });
         } catch (SQLException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         }
 
     }
-
 }
