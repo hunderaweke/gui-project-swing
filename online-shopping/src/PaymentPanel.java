c78e210106f1dc3745d8b5313cac5f54e05640e0
Kidus Hailu <kidustheastronomer@gmail.com>
2023-10-16 22:18:28 +0300

    all

---
+++ online-shopping/src/PaymentPanel.java
@@ -0,0 +1,57 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class PaymentPanel extends JPanel {
+    private JTextField amountTextField;
+    private JTextField balanceTextField;
+    private JTextField priceTextField;
+    private JButton payButton;
+
+    public PaymentPanel() {
+        setLayout(new GridLayout(4, 2, 10, 10));
+
+        JLabel amountLabel = new JLabel("Amount to Pay:");
+        amountTextField = new JTextField(10);
+
+        JLabel balanceLabel = new JLabel("Balance:");
+        balanceTextField = new JTextField(10);
+        balanceTextField.setEditable(false);
+
+        JLabel priceLabel = new JLabel("Price:");
+        priceTextField = new JTextField(10);
+
+        payButton = new JButton("Pay");
+        payButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                double amount = Double.parseDouble(amountTextField.getText());
+                double price = Double.parseDouble(priceTextField.getText());
+                double balance = amount - price;
+                balanceTextField.setText(String.format("%.2f", balance));
+            }
+        });
+
+        add(amountLabel);
+        add(amountTextField);
+        add(balanceLabel);
+        add(balanceTextField);
+        add(priceLabel);
+        add(priceTextField);
+        add(new JLabel()); // Empty label for spacing
+        add(payButton);
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Payment Panel");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300, 200);
+        frame.setLocationRelativeTo(null); // Center the frame on the screen
+
+        PaymentPanel paymentPanel = new PaymentPanel();
+        frame.add(paymentPanel);
+
+        frame.setVisible(true);
+    }
+}
---
+++ online-shopping/src/SignupPage.java
@@ -0,0 +1,107 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.*;
+
+
+public class SignupPage extends JFrame {
+    private JTextField usernameField;
+    private JTextField emailField;
+    private JPasswordField passwordField;
+    private JButton signupButton;
+
+    public SignupPage() {
+        setTitle("Signup");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setExtendedState(JFrame.MAXIMIZED_BOTH); // Set to full screen
+
+        JPanel contentPane = new JPanel();
+        contentPane.setBackground(new Color(173, 216, 230)); // Set light blue background color
+        setContentPane(contentPane);
+        contentPane.setLayout(new GridBagLayout());
+
+        JLabel headerLabel = new JLabel("Online Shopping");
+        headerLabel.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 24));
+        headerLabel.setForeground(Color.WHITE); // Set white text color
+
+        JLabel usernameLabel = new JLabel("Username:");
+        usernameField = new JTextField(20);
+
+        JLabel emailLabel = new JLabel("Email:");
+        emailField = new JTextField(20);
+
+        JLabel passwordLabel = new JLabel("Password:");
+        passwordField = new JPasswordField(20);
+
+        signupButton = new JButton("Signup");
+        signupButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String username = usernameField.getText();
+                String email = emailField.getText();
+                String password = new String(passwordField.getPassword());
+
+                // Establish database connection
+                try (Connection connection = DriverManager.getConnection("jdbc:sqlserver://your-server:1433;databaseName=your-database", "your-username", "your-password")) {
+                    // Prepare SQL statement
+                    String sql = "INSERT INTO users (username, email, password) VALUES (?, ?, ?)";
+                    try (PreparedStatement statement = connection.prepareStatement(sql)) {
+                        statement.setString(1, username);
+                        statement.setString(2, email);
+                        statement.setString(3, password);
+
+                        // Execute the SQL statement
+                        int rowsInserted = statement.executeUpdate();
+                        if (rowsInserted > 0) {
+                            JOptionPane.showMessageDialog(null, "Signup successful!");
+                        }
+                    }
+                } catch (SQLException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        });
+
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.insets = new Insets(10, 10, 10, 10); // Add some spacing
+        contentPane.add(headerLabel, gbc);
+
+        gbc.gridy = 1;
+        contentPane.add(usernameLabel, gbc);
+
+        gbc.gridx = 1; // Use the next column
+        contentPane.add(usernameField, gbc);
+
+        gbc.gridy = 2;
+        gbc.gridx = 0; // Reset to the first column
+        contentPane.add(emailLabel, gbc);
+
+        gbc.gridx = 1;
+        contentPane.add(emailField, gbc);
+
+        gbc.gridy = 3;
+        gbc.gridx = 0;
+        contentPane.add(passwordLabel, gbc);
+
+        gbc.gridx = 1;
+        contentPane.add(passwordField, gbc);
+
+        gbc.gridy = 4;
+        gbc.gridx = 0;
+        gbc.gridwidth = 2; // Span across two columns
+        contentPane.add(signupButton, gbc);
+
+        pack();
+        setLocationRelativeTo(null);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            SignupPage signupPage = new SignupPage();
+            signupPage.setVisible(true);
+        });
+    }
+}
