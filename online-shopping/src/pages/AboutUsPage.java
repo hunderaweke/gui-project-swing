54363ac68bbead3ccf2b3180634bfd5cbf7d35c6
Kidus Hailu <kidustheastronomer@gmail.com>
2023-10-17 22:02:14 +0300

    updated:  about us

--- online-shopping/src/pages/AboutUsPage.java
+++ online-shopping/src/pages/AboutUsPage.java
@@ -10,26 +10,29 @@ import custom_components.CustomLabel;
 public class AboutUsPage extends JPanel {
 
     public AboutUsPage() {
-        var groupMember1 = new CustomLabel("Hundera Awoke");
-        var groupMember2 = new CustomLabel("Daniel Merga");
-        var groupMember3 = new CustomLabel("Kidus Hailu");
-        var groupMember4 = new CustomLabel("Hermela Girma");
-        var groupMember5 = new CustomLabel("Yididiya Tesfaye");
-        var groupMember6 = new CustomLabel("Lelo Mohammed");
+        var groupHeading = new CustomLabel("Name                ID");
+        var groupMember1 = new CustomLabel("Hundera Awoke            UGR/25358/14");
+        var groupMember2 = new CustomLabel("Daniel Merga             UGR/25699/14");
+        var groupMember3 = new CustomLabel("Kidus Hailu              UGR/25309/14");
+        var groupMember4 = new CustomLabel("Hermela Girma            UGR/26306/14");
+        var groupMember5 = new CustomLabel("Yididiya Tesfaye         UGR/25451/14");
+        var groupMember6 = new CustomLabel("Lelo Mohammed            UGR/25557/14");
         setLayout(new GridBagLayout());
         var gbc = new GridBagConstraints();
         gbc.gridx = 0;
         gbc.gridy = 0;
-        add(groupMember1, gbc);
+        add(groupHeading, gbc);
         gbc.gridy = 1;
-        add(groupMember2, gbc);
+        add(groupMember1, gbc);
         gbc.gridy = 2;
-        add(groupMember3, gbc);
+        add(groupMember2, gbc);
         gbc.gridy = 3;
-        add(groupMember4, gbc);
+        add(groupMember3, gbc);
         gbc.gridy = 4;
-        add(groupMember5, gbc);
+        add(groupMember4, gbc);
         gbc.gridy = 5;
+        add(groupMember5, gbc);
+        gbc.gridy = 6;
         add(groupMember6, gbc);
 
     }
