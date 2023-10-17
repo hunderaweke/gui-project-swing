935ba67e6db5c6194256dd3f536e7250c92fba19
hunderaweke <hunderaweke@gmail.com>
2023-10-17 04:06:46 -0700

    updated: sidebar button

--- online-shopping/src/custom/SideBarButton.java
+++ online-shopping/src/custom/SideBarButton.java
@@ -2,26 +2,11 @@ package custom;
 
 import java.awt.*;
 
-import javax.swing.ButtonModel;
-import javax.swing.event.ChangeEvent;
-import javax.swing.event.ChangeListener;
-
 public class SideBarButton extends CustomButton {
     public SideBarButton(String text) {
         super(text);
         setBackground(new Color(143, 196, 212));
         setForeground(Color.BLACK);
-        getModel().addChangeListener(new ChangeListener() {
-            @Override
-            public void stateChanged(ChangeEvent e) {
-                var model = (ButtonModel) e.getSource();
-                if (model.isRollover()) {
-                    setBackground(new Color(205, 234, 226));
-                } else {
-                    setBackground(new Color(143, 196, 212));
-                }
-            }
-        });
 
     }
 }
