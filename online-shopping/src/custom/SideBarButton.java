59e32c3fe2bab56056d33b8c6300db7e7394cb4b
natanmuletahunde <natanmuleta911@gmail.com>
2023-10-17 03:50:48 -0700

    updated: unused imports

--- online-shopping/src/custom/SideBarButton.java
+++ online-shopping/src/custom/SideBarButton.java
@@ -1,9 +1,5 @@
 package custom;
 
-import javax.swing.*;
-import javax.swing.event.ChangeEvent;
-import javax.swing.event.ChangeListener;
-
 import java.awt.*;
 
 public class SideBarButton extends CustomButton {
@@ -12,17 +8,17 @@ public class SideBarButton extends CustomButton {
         this.setBackground(new Color(143, 196, 212));
         // this.setForeground(Color.BLACK);
         // this.getModel().addChangeListener(new ChangeListener() {
-        //     @Override
-        //     public void stateChanged(ChangeEvent e) {
-        //         ButtonModel model = (ButtonModel) e.getSource();
-        //         if (model.isRollover()) {
-        //             setBackground(new Color(205, 234, 226));
-        //             setForeground(Color.BLACK);
-        //         } else {
-        //             setBackground(new Color(143, 196, 212));
-        //             setForeground(Color.BLACK);
-        //         }
-        //     }
+        // @Override
+        // public void stateChanged(ChangeEvent e) {
+        // ButtonModel model = (ButtonModel) e.getSource();
+        // if (model.isRollover()) {
+        // setBackground(new Color(205, 234, 226));
+        // setForeground(Color.BLACK);
+        // } else {
+        // setBackground(new Color(143, 196, 212));
+        // setForeground(Color.BLACK);
+        // }
+        // }
 
         // });
 
